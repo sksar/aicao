df6520da7035d73e0185f26326d72cbb830552eb
lksar <lksar70@gmail.com>
2024-05-06 22:56:57 +0530

    Annexure

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div class="text-center">
+    <h1 class="text-3xl font-bold my-5">Contact</h1>
+</div>
---
+++ src/routes/forms/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="text-center">
+    <h1 class="text-3xl font-bold my-5"></h1>
+</div>
+<div class="prose max-w-full">
+    <h2>Forms</h2>
+</div>
+Authorisation for deduction of Subscription by DDO
+
+<a
+    href="https://cms.aicao.in/api/files/wibitekxf6rd9m8/av3wynja9orgi5u/cao_subscription_form_RxHjhQLJlZ.pdf"
+    style="color:blue">Annexure-I</a
+>
+
+<style lang="postcss">
+    h2 {
+        @apply border-b-2 border-blue-300;
+    }
+</style>
